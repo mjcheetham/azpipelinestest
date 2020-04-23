5b8b685bfe410c7364a0f4117ecfe9591b15ac6f
Matthew John Cheetham <mattche@microsoft.com>
2020-04-23 11:00:44 +0100

    initial commit

---
+++ testproj.tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace testproj.tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.NotNull(new Class1());
+        }
+    }
+}
---
+++ testproj/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace testproj
+{
+    public class Class1
+    {
+        public Class1()
+        {
+            var t = typeof(JsonConvert);
+        }
+    }
+}
